a3fd3ec988de01f797fd6c61dd61f5934cbb9965
Ilia-Tempest <51461710+Ilia-Tempest@users.noreply.github.com>
2019-10-06 16:38:48 +0500

    form test

--- src/form-lesson/mail.php
+++ src/form-lesson/mail.php
@@ -16,7 +16,7 @@ $mail->SMTPAuth = true;                               // Enable SMTP authenticat
 $mail->Username = 'iparshacov@gmail.com'; // Ваш логин от почты с которой будут отправляться письма
 $mail->Password = 'Arialdragon47'; // Ваш пароль от почты с которой будут отправляться письма
 $mail->SMTPSecure = 'ssl';                            // Enable TLS encryption, `ssl` also accepted
-$mail->Port = 465; // TCP port to connect to / этот порт может отличаться у других провайдеров
+$mail->Port = 587; // TCP port to connect to / этот порт может отличаться у других провайдеров
 
 $mail->setFrom('iparshacov@gmail.com'); // от кого будет уходить письмо?
 $mail->addAddress('webitrab@gmail.com');     // Кому будет уходить письмо 

56b77b61e5b6be8259ac334a88aaa16545a80d3b
Ilia-Tempest <51461710+Ilia-Tempest@users.noreply.github.com>
2019-10-13 22:06:14 +0500

    ALL

---
+++ src/form/form.php
@@ -0,0 +1,37 @@
+<?php 
+
+require_once('PHPMailer/PHPMailerAutoload.php');
+$mail = new PHPMailer;
+$mail->CharSet = 'utf-8';
+
+$phone = $_POST['user_phone'];
+
+//$mail->SMTPDebug = 3;                               // Enable verbose debug output
+
+$mail->isSMTP();                                      // Set mailer to use SMTP
+$mail->Host = 'ssl://smtp.yandex.ru';  																							// Specify main and backup SMTP servers
+$mail->SMTPAuth = true;                               // Enable SMTP authentication
+$mail->Username = 'schumakdmitry@yandex.ru'; // Ваш логин от почты с которой будут отправляться письма
+$mail->Password = '1r!sL4Tt'; // Ваш пароль от почты с которой будут отправляться письма                           // Enable TLS encryption, `ssl` also accepted
+$mail->Port = 465; // TCP port to connect to / этот порт может отличаться у других провайдеров
+
+$mail->setFrom('schumakdmitry@yandex.ru'); // от кого будет уходить письмо?
+$mail->addAddress('zakaz@alta-m.spb.ru');     // Кому будет уходить письмо 
+//$mail->addAddress('ellen@example.com');               // Name is optional
+//$mail->addReplyTo('info@example.com', 'Information');
+//$mail->addCC('cc@example.com');
+//$mail->addBCC('bcc@example.com');
+//$mail->addAttachment('/var/tmp/file.tar.gz');         // Add attachments
+//$mail->addAttachment('/tmp/image.jpg', 'new.jpg');    // Optional name
+$mail->isHTML(true);                                  // Set email format to HTML
+
+$mail->Subject = 'Заявка с сайта "ООО АЛЬТА М" сайта';
+$mail->Body    = '<br>Пользователь оставил заявку, его телефон: ' .$phone;
+$mail->AltBody = '';
+
+if(!$mail->send()) {
+    echo 'Error';
+} else {
+    header('location: ../index.html.overlay');
+}
+?>
